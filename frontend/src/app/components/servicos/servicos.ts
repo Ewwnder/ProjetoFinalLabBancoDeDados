74c178bb3cad15867ffd1f3f27d91880efa457ca
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-11-27 10:13:40 -0300

    feat(front): adiciona service de Gerenciamento de Serviços

--- frontend/src/app/components/servicos/servicos.ts
+++ frontend/src/app/components/servicos/servicos.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Servico } from '../../../entity/servico';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-servicos',
