1cf36cbd442394285976a290ab910ebac32f9b40
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-27 10:50:09 -0300

    :sparkles: feat: tela de agendamento, versao inicial

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrdemServico } from './components/ordem-servico/ordem-servico';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OrdemServico],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
 /// <reference types="@angular/localize" />
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
