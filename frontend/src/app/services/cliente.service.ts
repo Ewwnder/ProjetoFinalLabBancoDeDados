0e8633da93886e91c5a3a67997ebad85680abe6d
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-12-09 17:07:50 -0300

    feat(Cliente): adiciona service de Cliente

--- frontend/src/app/services/cliente.service.ts
+++ frontend/src/app/services/cliente.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../../entity/cliente';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/clientes'; 
+
+  constructor(private http: HttpClient) {}
+
+  filtrar(busca?: string, ordenarAZ?: boolean): Observable<Cliente[]> {
+    let parametros = new HttpParams();
+
+    if (busca) {
+      parametros = parametros.set('busca', busca);
+    }
+
+    if (ordenarAZ !== undefined) {
+      parametros = parametros.set('ordenarAZ', ordenarAZ.toString());
+    }
+
+    return this.http.get<Cliente[]>(this.apiUrl, { params: parametros });
+  }
+
+  salvar(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.apiUrl, cliente);
+  }
+
+  atualizar(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.apiUrl}/${cliente.id}`, cliente);
+  }
+
+  excluir(cliente: Cliente): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${cliente.id}`);
+  }
 }
