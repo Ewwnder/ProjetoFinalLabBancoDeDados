2ddb9210df94e7269d9b7111e8d063d2e50db376
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-12-10 08:34:02 -0300

    feat(Home): adiciona home do projeto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Servicos } from './components/servicos/servicos';
 import { OrdemServico } from './components/ordem-servico/ordem-servico';
 import { ResponsavelComponent } from './components/responsavel/responsavel.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'servicos', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
+  { path: 'home', component: HomeComponent},
   { path: 'servicos', component: Servicos },
   { path: 'ordem-servico', component: OrdemServico },
   { path: 'responsavel', component: ResponsavelComponent },
