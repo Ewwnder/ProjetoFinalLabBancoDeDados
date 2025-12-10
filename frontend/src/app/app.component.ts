f9856bafa3855e79a27437d62ad33dfe5e414728
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-12-10 08:45:11 -0300

    feat(NavBar): adiciona navbar e ajustes conflitos nas rotas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component'; 
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { OrdemServico } from './components/ordem-servico/ordem-servico';
 import { ResponsavelComponent } from './components/responsavel/responsavel.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   { path: 'home', component: HomeComponent},
+  { path: 'navbar', component: NavbarComponent},
   { path: 'servicos', component: Servicos },
   { path: 'ordem-servico', component: OrdemServico },
   { path: 'responsavel', component: ResponsavelComponent },
