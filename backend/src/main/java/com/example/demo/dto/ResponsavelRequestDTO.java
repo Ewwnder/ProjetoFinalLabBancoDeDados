4b15ba1b80a237d927e18c919331576acfe28abf
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-12-10 17:33:38 -0300

    feat(Responsavel): adiciona DTO Responsavel

---
+++ backend/src/main/java/com/example/demo/dto/ResponsavelRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+public record ResponsavelRequestDTO(
+        String nome,
+        String email,
+        String telefone,
+        String cargo,
+        String especialidade,
+        Double salario
+) {}
